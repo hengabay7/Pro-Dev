1de0c26fba04eaf07415ecc45424356e3721fcc7
hen gabay <hengabay@.co.il>
2025-02-25 20:19:16 +0200

    commited

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -116,18 +116,13 @@ module "ec2_instance" {
 
 resource "aws_eip" "eip" {
   domain = "vpc"
-}
 
-tags = {
-  Name = "MyElasticIP"  
+  tags = {
+    Name = "MyElasticIP"  
+  }
 }
 
 resource "aws_eip_association" "eip_attach" {
   instance_id   = module.ec2_instance.id
   allocation_id = aws_eip.eip.id
-}
-
-output "external_ip" {
-  value       = aws_eip.eip.public_ip
-  description = "The Elastic IP assigned to the EC2 instance"
 }
